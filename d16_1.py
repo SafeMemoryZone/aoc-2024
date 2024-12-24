40507508f05a83bae5cd1211f95f0ccfe6cf297f
Viliam Holly <viliamteoholly@gmail.com>
2024-12-24 10:27:03 +0100

    Day 16 part 1

---
+++ d16_1.py
@@ -0,0 +1,47 @@
+import heapq
+
+offsets = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+def find_cost(grid):
+    distances = []
+    end_pos = tuple()
+
+    for row_idx, line in enumerate(grid):
+        for col_idx, c in enumerate(line):
+            if c == "S":
+                heapq.heappush(distances, (0, row_idx, col_idx, (0, 1)))
+            elif c == "E":
+                end_pos = (row_idx, col_idx)
+            elif c != ".":
+                heapq.heappush(distances, (float("inf"), row_idx, col_idx, None))
+
+    visited = set()
+
+    while len(distances) != 0:
+        dist, curr_row_idx, curr_col_idx, curr_dir = heapq.heappop(distances)
+
+        if (curr_row_idx, curr_col_idx, curr_dir) in visited:
+            continue
+
+        visited.add((curr_row_idx, curr_col_idx, curr_dir))
+
+        if curr_row_idx == end_pos[0] and curr_col_idx == end_pos[1]:
+            return dist
+        
+        for off in offsets:
+            next_row_idx = curr_row_idx + off[0]
+            next_col_idx = curr_col_idx + off[1]
+
+            if grid[next_row_idx][next_col_idx] == "#":
+                continue
+
+            cost = dist + 1 if off == curr_dir else dist + 1001
+
+            heapq.heappush(distances, (cost, next_row_idx, next_col_idx, off))
+
+    return -1 # no path
+
+with open("inp", "r") as inp:
+    grid = [list(x.strip()) for x in inp.read().split("\n")]
+
+    print(find_cost(grid))
